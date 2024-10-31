fc49a023bd844434fd789599e42aa118bca15540
AitezazAhmed <stoneghold4103@gmail.com>
2024-10-31 22:21:26 +0500

    01_Basic Mathematical Operations.js

---
+++ Build A Pessanger Counter/01_Basic Mathematical Operations.js
@@ -0,0 +1,19 @@
+let count = 3 + 7
+console.log(count)
+// there are many mathematical operators
+// + (addition) -(minus) /(division) *(multiplication)  ** (exponent,power) 
+let FirstBatch=10
+let SecondBatch=17
+let Total=FirstBatch+SecondBatch
+console.log(Total)
+
+//Exercise
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+let MyAge=17
+let humanDogRatio=7
+let myDogAge=MyAge*humanDogRatio
+console.log(myDogAge)
