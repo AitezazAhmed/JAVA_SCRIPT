af44899997a12d0ad1268d4f317a4b0fa869d6e5
AitezazAhmed <stoneghold4103@gmail.com>
2024-10-31 22:36:26 +0500

    02_Reassigning and incrementing.js

---
+++ Build A Pessanger Counter/02_Reassigning and incrementing.js
@@ -0,0 +1,25 @@
+let count = 5
+count=2
+console.log(count)
+//the answer is 2 beacuse js assign the latest value of count this is called Reassinging
+
+// increment
+let number = 10
+number =10+2
+console.log(number)
+// the answer is 12 because number increase by 2 this is called incrementing
+
+// exercise
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+
+let bonuspoints = 50
+console.log(bonuspoints)
+bonuspoints=bonuspoints+50
+console.log(bonuspoints)
+bonuspoints=bonuspoints-75
+console.log(bonuspoints)
+bonuspoints=bonuspoints+45
+console.log(bonuspoints)
+
+
