20f68183e9b75cc3f7b8390773c1ddb1273a17c1
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-01 11:44:44 +0500

    06_Using functions to write less code.js

---
+++ Build A Pessanger Counter/06_Using functions to write less code.js
@@ -0,0 +1,30 @@
+function race(){
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+// we use funtion to write less code for example we put all the console in function when ever we call this funtion console exectue which is present in function
+
+// Setting up the the race 🏎 🏎 🏎
+race()
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+// Get ready for a new race 🏎 🏎 🏎
+race()
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 
+
+//exercise
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+function number(){
+  console.log(42)
+}
+
+number()
