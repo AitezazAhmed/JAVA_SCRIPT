ca58c6084702ef494b3e32a034ddc603a67114a6
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-01 11:26:22 +0500

    05_The onclick event listener.js

---
+++ Build A Pessanger Counter/05_The onclick event listener.js
@@ -0,0 +1,13 @@
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count in the HTML to reflect the new count
+
+function increment(){
+    console.log("The button was clicked")
+}
