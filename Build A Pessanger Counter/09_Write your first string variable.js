4de2c5d7957f7a715d71709777e31bfd014e5a33
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-02 13:20:36 +0500

    09_Write your first string variable.js

---
+++ Build A Pessanger Counter/09_Write your first string variable.js
@@ -0,0 +1,26 @@
+//"per" string we use double quotes or single quotes for string like that'per'
+let username="per"
+// console.log(username)
+
+//exercise
+// Create a variable, message, that stores the string: "You have tree new notifications"
+let message="You have tree new notifications"
+// console.log(message+","+username+"!")
+
+// Create a variable, messageToUser, that contains the message we have logged
+let messageToUser=(message+","+username+"!")
+console.log(messageToUser)
+
+//Exercise 
+
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+let name="Aitezaz"
+let greeting="Hi, my name is "
+let myGreeting=(greeting+name)
+console.log(myGreeting)
+
