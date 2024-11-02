84e1bc5cd77f327b453503e18f1d0eab82d5bf28
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-02 13:34:53 +0500

    10_Strings vs. Numbers.js

---
+++ Build A Pessanger Counter/10_Strings vs. Numbers.js
@@ -0,0 +1,15 @@
+let name=42
+greeting="hi i am "
+// console.log(greeting+name) // answer is hi i am 42 it is because sting have more priority then number ans 42 also becone string like that "hi i am 42"
+let points=4
+bonouspoints="10"
+// console.log(points+bonouspoints)// the answer is 410 it is because number cannot add to string and number attach with string and number chante into string
+//exercise
+// writer Answer before runing code
+
+console.log(4 + 5) // Answer is 9
+console.log("2" + "4") // Answer is 24
+console.log("5" + 1) //Answer is 51 
+console.log(100 + "100") //Answer is 100100
+
+//  All Answer is correct
