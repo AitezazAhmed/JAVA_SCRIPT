b4326b406b501e59a5ac783ab23b07995b97139a
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-15 21:04:32 +0500

    02_If...else conditionals.js

---
+++ Build A Blackjack Game/02_If...else conditionals.js
@@ -0,0 +1,12 @@
+let firstcard=3
+let secondcard=20
+let sum=firstcard+secondcard
+if (sum<23){
+    console.log("Do you want to draw a new card? 🙂")
+}
+else if (sum===23){
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}
+else{
+    console.log("You're out of the game! 😭")
+}
