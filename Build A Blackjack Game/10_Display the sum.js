9de966c9ae45e5f4f933642f167e4d4d1c198fdb
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-17 21:48:56 +0500

    10_Display the sum.js

---
+++ Build A Blackjack Game/10_Display the sum.js
@@ -0,0 +1,24 @@
+let messageEl=document.getElementById("message-el")
+ let sumEl = document.getElementById("sum-el")
+let firstcard=22
+let secondcard=1
+let sum=firstcard+secondcard
+let hasBlackjack=false
+let isAlive=true
+message=""
+function startGame(){
+    if (sum<23){
+        message="Do you want to draw a new card?"
+    }
+    else if (sum===23){
+        message="Wohoo! You've got Blackjack!"
+        hasBlackjack=true
+    }
+    else{
+        message="You're out of the game!"
+        isAlive=false
+    }
+    messageEl.innerHTML=message
+    sumEl.innerHTML="Sum:"+sum
+
+}
