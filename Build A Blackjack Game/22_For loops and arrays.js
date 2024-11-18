2e975bc4780805819438911944982aea55b9afbb
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-18 22:02:36 +0500

    22_For loops and arrays.js

---
+++ Build A Blackjack Game/22_For loops and arrays.js
@@ -0,0 +1,14 @@
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!"
+]
+// DRY - Don't repeat yourself
+// console.log(messages[0])
+// console.log(messages[1])
+// console.log(messages[2])
+// console.log(messages[3])
+for(i=0; i<5; i+=1){
+    console.log(messages[i])// here we use i for index
+}
