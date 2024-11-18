28053c080eed20fd71dddeb37122842a919ecde8
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-18 20:26:07 +0500

    17_Arrays with multiple data types.js

---
+++ Build A Blackjack Game/17_Arrays with multiple data types.js
@@ -0,0 +1,11 @@
+// Array - ordered list of items - coposite / complex data type
+
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+let array=["Aitezaz",
+    "18",
+    hasyoulikepizza=true
+]
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
