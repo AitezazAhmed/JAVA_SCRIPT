41a94ced7f3917e7966d0f4846f533c68d21d0ec
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-21 00:06:24 +0500

    28_Math.random().js

---
+++ Build A Blackjack Game/28_Math.random().js
@@ -0,0 +1,12 @@
+let random=Math.random()
+console.log(random)
+
+
+
+/* 
+
+What does Math.random() do?
+
+Your answer: 
+Your answer: it generates a random number between 0 and 1 (not inclusive of 1)
+*/
