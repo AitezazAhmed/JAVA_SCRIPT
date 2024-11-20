5bd9fa2f32d024ccc6341644e420f5a108d0594d
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-21 00:06:50 +0500

    31_dice in function.js

---
+++ Build A Blackjack Game/31_dice in function.js
@@ -0,0 +1,6 @@
+// Create a function, rollDice(), that returns a random number between 1 and 6
+function rolldice(){
+    let dice=Math.floor( Math.random()*7)
+    return dice
+}
+console.log(rolldice())
