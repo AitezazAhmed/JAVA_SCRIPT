567ab90584f4609e2128a29bd1eb6a7bdd06d4b8
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-21 01:28:42 +0500

    33_Complete getRandomNumber function.js

---
+++ Build A Blackjack Game/33_Complete getRandomNumber function.js
@@ -0,0 +1,48 @@
+let messageEl=document.getElementById("message-el")
+ let sumEl = document.getElementById("sum-el")
+ let cardEl=document.getElementById("card-el")
+let firstcard=getrandoncard()
+let secondcard=getrandoncard()
+let cards=[firstcard,secondcard]
+let sum=firstcard+secondcard
+let hasBlackjack=false
+let isAlive=true
+message=""
+function getrandoncard(){
+    let random=Math.floor(Math.random()*14)
+    if (random>10){
+        return 10
+    } else if(random===1){
+        return 11
+    }else{
+        return random
+    }
+}
+function startGame(){
+    rendergame()
+}
+function rendergame(){
+    if (sum<23){
+        message="Do you want to draw a new card?"
+    }
+    else if (sum===23){
+        message="Wohoo! You've got Blackjack!"
+        hasBlackjack=true
+    }
+    else{
+        message="You're out of the game!"
+        isAlive=false
+    }
+    messageEl.innerHTML=message
+    sumEl.innerHTML="Sum:"+sum
+    cardEl.innerHTML="Cards:"
+    for(i=0; i<cards.length; i++){
+        cardEl.innerHTML+=cards[i]+" "
+        }
+}
+function newcard() {
+    let card = getrandoncard()
+    sum += card
+    cards.push(card)
+    rendergame()
+}
