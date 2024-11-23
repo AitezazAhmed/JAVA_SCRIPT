65f640d68bc31e721a2be5714f9307fb4205113c
AitezazAhmed <stoneghold4103@gmail.com>
2024-11-23 08:12:12 +0500

    35_The AND operator (&&).js

---
+++ Build A Blackjack Game/35_The AND operator (&&).js
@@ -0,0 +1,8 @@
+let hascompletecourse=true
+let givethecertificate=true
+if(hascompletecourse===true&&givethecertificate===true){
+Generating()
+}
+
+function Generating(){
+console.log("Generating certificate....")}
